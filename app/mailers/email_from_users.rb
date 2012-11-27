d97eb74af4f4ed6e839a93c2132ef6c39e87c277
Isaac Betesh <iybetesh@gmail.com>
2012-11-27 18:40:07 -0500

    `rails generate mailer email_from_users contact_us`

---
+++ app/mailers/email_from_users.rb
@@ -0,0 +1,14 @@
+class EmailFromUsers < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.email_from_users.contact_us.subject
+  #
+  def contact_us
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
