1f897d9a9f344db5fc95ab6adb35966f0f83950b
Isaac Betesh <iybetesh@gmail.com>
2012-12-03 14:28:28 -0500

    created home page with home, who we are, and documents tab

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,33 @@
 module MainHelper
+  def centered_title text
+    (content_tag :h2, :class => 'four columns centered' do; text; end).html_safe
+  end
+  def eight_columns_centered &block
+    (content_tag :div, :class => 'row' do
+      content_tag :div, :class => 'eight columns centered' do
+        yield
+      end
+    end).html_safe
+  end
+  def partial_in_tab id, template=nil, active=nil
+    li_with_options id, active do
+      render :partial => 'content', :locals => { :content => id, :template => template }
+    end
+  end
+  def split_as_paragraphs text
+    simple_format(text.gsub(/\n/,"\n\n")).html_safe
+  end
+  def li_with_options id, active, &block
+    options = { :id => "#{id}Tab" }
+    options.merge!({ :class => :active }) if active && :active == active.to_sym
+    (content_tag :li, options do
+      yield
+    end).html_safe
+  end
+  def tab_for id
+    (content_tag :a, :href => "##{id}" do; title_for(id); end).html_safe
+  end
+  def title_for id
+    t(".#{id.to_s.underscore}_title").html_safe
+  end
 end
