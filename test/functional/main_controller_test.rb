d555ac4f453fa32cd360610dc0f8115ed7949444
Isaac Betesh <iybetesh@gmail.com>
2012-12-05 13:16:55 -0500

    implemented functional test for contact_us action

--- test/functional/main_controller_test.rb
+++ test/functional/main_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class MainControllerTest < ActionController::TestCase
+  def contact_params
+    result = {}
+    GanShelanu.config.contact_us_fields.each { |k|
+      result[k] = @contact.__send__(k)
+    }
+    result
+  end
   test "should get home" do
     get :home
     assert_response :success
   end
 
   test "should get contact_us" do
-    get :contact_us
+    post :contact_us, :contact => contact_params
     assert_response :success
   end
-
 end
